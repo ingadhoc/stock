02738e12c5ff5e4bfc59e9faa93c9067a439513a
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-06-13 11:33:13 -0300

    ADD stock picking state

---
+++ stock_picking_state/stock.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, fields
+
+
+class stock_picking_state_detail(models.Model):
+    _name = 'stock.picking.state_detail'
+    _order = 'sequence'
+
+    name = fields.Char('Name')
+    sequence = fields.Integer('Sequence')
+    picking_type = fields.Selection(
+        selection=[
+            ('internal', 'Internal'),
+            ('outgoing', 'Outgoing'),
+            ('incoming', 'Incoming'),
+        ],
+        string="Picking Type",
+    )
+    state = fields.Selection(
+        store=True,
+        selection=[
+            ('draft', 'Draft'),
+            ('cancel', 'Cancelled'),
+            ('waiting', 'Waiting Another Operation'),
+            ('confirmed', 'Waiting Availability'),
+            ('partially_available', 'Partially Available'),
+            ('assigned', 'Ready to Transfer'),
+            ('done', 'Transferred'),
+        ], string='Status'
+    )
+    picking_ids = fields.One2many(
+        'stock.picking',
+        'state_detail_id',
+        'Pickings')
+
+
+class stock_picking(models.Model):
+
+    _inherit = 'stock.picking'
+
+    state_detail_id = fields.Many2one(
+        'stock.picking.state_detail',
+        string='State Detail',
+        track_visibility='onchange',
+        select=True
+    )
+
+    code = fields.Selection(
+        related='picking_type_id.code')
