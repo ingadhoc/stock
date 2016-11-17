3283b87cb8e2bb63c83e096976c8e2a9efc4f6fb
Emiq2 <eq@adhoc.com.ar>
2016-11-17 10:53:50 -0300

    [9.0] MIG stock_picking_ean128_report

--- stock_picking_ean128_report/__openerp__.py
+++ stock_picking_ean128_report/__openerp__.py
@@ -24,11 +24,6 @@
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
-    'description': '''
-Stock Picking EAN128 Report
-===========================
-
-    ''',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -46,7 +41,7 @@ Stock Picking EAN128 Report
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
