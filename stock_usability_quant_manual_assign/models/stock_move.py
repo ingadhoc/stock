4696ba9d4adfc387ec0695bc885a06fe4e14cdcd
nicomacr <nmr@adhoc.com.ar>
2017-06-07 10:32:18 -0300

    FIX restrict lot and more opertation with lots

--- stock_usability_quant_manual_assign/models/stock_move.py
+++ stock_usability_quant_manual_assign/models/stock_move.py
@@ -3,5 +3,14 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import stock_move
-from . import stock_production_lot
+from openerp import models, fields
+
+
+class StockMove(models.Model):
+
+    _inherit = 'stock.move'
+
+    pack_operation_product_ids = fields.One2many(
+        'stock.pack.operation',
+        related='picking_id.pack_operation_product_ids',
+        readonly=True)
