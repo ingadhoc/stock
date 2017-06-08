e1def840abe05aae51025542bd532e6b9ecd8f45
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-06-08 20:37:12 -0300

    IMP stock transferrestrcti lot

--- stock_transfer_restrict_lot/models/stock_production_lot.py
+++ stock_transfer_restrict_lot/models/stock_production_lot.py
@@ -20,8 +20,11 @@ class StockProductionlot(models.Model):
     @api.depends('quant_ids.reservation_id', 'quant_ids.qty')
     def _compute_qty_available_not_res(self):
         for rec in self:
+            # consideramos available si location es internal solamente
             rec.qty_available_not_res = sum(rec.quant_ids.filtered(
-                lambda x: not x.reservation_id).mapped('qty'))
+                lambda x: (
+                    x.location_id.usage == 'internal' and
+                    not x.reservation_id)).mapped('qty'))
 
     @api.multi
     def name_get(self):
@@ -29,12 +32,14 @@ class StockProductionlot(models.Model):
         show_locations_qty = self.env.context.get('show_locations_qty')
         for rec in self:
             if show_locations_qty and rec.quant_ids:
-                locations = rec.quant_ids.mapped('location_id')
+                locations = rec.quant_ids.mapped('location_id').filtered(
+                    lambda x: x.usage == 'internal')
                 locations_qty = ['%s: %s' % (
                     location.name, sum(rec.quant_ids.filtered(
                         lambda x: x.location_id == location).mapped(
                         'qty'))) for location in locations]
-                name = '%s (%s)' % (rec.name, ', '.join(locations_qty))
+                name = locations and '%s (%s)' % (
+                    rec.name, ', '.join(locations_qty)) or rec.name
             else:
                 name = rec.name
             result.append((rec.id, name))
