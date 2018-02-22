e9c9615b504465a63e7d71277aa4544fd8a08bb8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-02-22 12:26:35 -0300

    IMP location security performance

--- location_security/models/stock.py
+++ location_security/models/stock.py
@@ -11,17 +11,16 @@ from openerp.exceptions import UserError
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    @api.one
+    @api.multi
     @api.constrains('state', 'location_id', 'location_dest_id')
     def check_user_location_rights(self):
-        if self.state == 'draft':
-            return True
-        user_locations = self.env.user.stock_location_ids
         if self.env.user.restrict_locations:
+            user_locations = self.env.user.stock_location_ids
             message = _(
                 'Invalid Location. You cannot process this move since you do '
                 'not control the location "%s".')
-            if self.location_id not in user_locations:
-                raise UserError(message % self.location_id.name)
-            elif self.location_dest_id not in user_locations:
-                raise UserError(message % self.location_dest_id.name)
+            for rec in self.filtered(lambda x: x.state != 'draft'):
+                if rec.location_id not in user_locations:
+                    raise UserError(message % rec.location_id.name)
+                elif rec.location_dest_id not in user_locations:
+                    raise UserError(message % rec.location_dest_id.name)
