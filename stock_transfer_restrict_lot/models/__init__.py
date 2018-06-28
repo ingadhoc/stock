06834e46542f8422b24105cf1912a1e0b2e7ef11
Valentino <vd@adhoc.com.ar>
2018-06-28 13:06:25 -0300

    [REM] stock_transfer_restrict_lot deprecated for V11.

--- stock_transfer_restrict_lot/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_pack_operation
-from . import stock_production_lot
