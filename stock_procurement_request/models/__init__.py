37b5d72b4f9362b74a6d599cba11cf08a5836f63
Valentino <vd@adhoc.com.ar>
2018-07-24 08:50:44 -0300

    [REM] stock_procurement_request deprecated for V11.

--- stock_procurement_request/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_procurement_request
-from . import procurement_order
-from . import stock_move
