d92dcc368fb6c415ae788b023779734ec75176c3
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-08-31 16:12:42 -0300

    [REL] Migration: stock_picking_ean128_report to 11.0 (#105)
    * [FIX] Update Readme and Manifest
    * [FIX] rename xml and .py files according guidelines
    * [FIX] identation and some fixes

---
+++ stock_picking_ean128_report/wizards/stock_lot_print_ean128_report.py
@@ -0,0 +1,42 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, api, models
+
+
+class StockLotEan128Report(models.TransientModel):
+
+    _name = 'stock.lot.print_ean128_report'
+
+    @api.model
+    def _get_lot(self):
+        active_id = self._context.get('active_id', False)
+        return self.env['stock.production.lot'].browse(active_id)
+
+    lot_id = fields.Many2one(
+        'stock.production.lot',
+        default=lambda self: self._get_lot(),
+    )
+    quantity = fields.Integer(
+        default=1,
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        related='lot_id.product_id',
+        readonly=True,
+    )
+
+    @api.multi
+    def do_print_report(self):
+        self.ensure_one()
+        return self.env['ir.actions.report'].search(
+            [('report_name', '=', 'report_stock_lot_EAN128')],
+            limit=1).report_action(self)
+
+    @api.multi
+    def do_print_report_excel(self):
+        self.ensure_one()
+        return self.env['ir.actions.report'].search(
+            [('report_name', '=', 'report_stock_lot_EAN128_excel')],
+            limit=1).report_action(self)
---
+++ stock_picking_ean128_report/wizards/stock_picking_print_ean128_report.py
@@ -0,0 +1,47 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import fields, api, models
+
+
+class StockPickingEan128Report(models.TransientModel):
+    _name = 'stock.picking.print_ean128_report'
+
+    stock_picking_line_ids = fields.One2many(
+        'stock.picking.print_ean128_report_detail',
+        'stock_picking_report_id',
+        'Product Print',
+        default=lambda self: self._get_stock_picking_line(),
+    )
+
+    @api.model
+    def _get_stock_picking_line(self):
+        active_id = self._context.get('active_id', False)
+        picking = self.env['stock.picking'].browse(active_id)
+        if picking.move_line_ids:
+            lines = []
+            for line in picking.move_line_ids.filtered('lot_id'):
+                values = {
+                    'product_id': line.product_id.id,
+                    'quantity': line.qty_done,
+                    'lot_id': line.lot_id.id,
+                    'product_uom_id': line.product_uom_id.id,
+                }
+                lines.append((0, 0, values))
+            return lines
+        return False
+
+    @api.multi
+    def do_print_report(self):
+        self.ensure_one()
+        return self.env['ir.actions.report'].search(
+            [('report_name', '=', 'report_stock_picking_EAN128')],
+            limit=1).report_action(self)
+
+    @api.multi
+    def do_print_report_excel(self):
+        self.ensure_one()
+        return self.env['ir.actions.report'].search(
+            [('report_name', '=', 'report_stock_picking_EAN128_excel')],
+            limit=1).report_action(self)
