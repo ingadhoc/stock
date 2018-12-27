7c501739e254a1181f08c5b003a02a794666bf3f
nicomacr <nmr@adhoc.com.ar>
2018-12-27 12:33:14 -0300

    [IMP] set all done in pickings only setted the qty that has reserved for this move. if has not reserved qty for the move, set the intial demand

--- stock_ux/models/stock_move_line.py
+++ stock_ux/models/stock_move_line.py
@@ -4,6 +4,7 @@
 ##############################################################################
 from odoo import models, api, fields, _
 from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
 
 
 class StockMoveLine(models.Model):
@@ -36,8 +37,14 @@ class StockMoveLine(models.Model):
 
     @api.multi
     def set_all_done(self):
+        precision = self.env['decimal.precision'].precision_get(
+            'Product Unit of Measure')
         for rec in self:
-            rec.qty_done = rec.move_id.product_uom_qty
+            rec.qty_done = rec.move_id.reserved_availability\
+                if not float_is_zero(
+                    rec.move_id.reserved_availability,
+                    precision_digits=precision) else\
+                rec.move_id.product_uom_qty
         if self._context.get('from_popup', False):
             return self[0].move_id.action_show_details()
 
