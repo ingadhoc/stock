2ab2b8bf47144ba0821c32e09f7c4233587e0ac1
nicomacr <nmr@adhoc.com.ar>
2019-01-11 15:40:54 -0300

    [IMP] changes in calculation for  inventory valuation in quants if the currency of product and quant are different

--- stock_usability/models/stock_quant.py
+++ stock_usability/models/stock_quant.py
@@ -9,6 +9,23 @@ from openerp import models, api
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
+    def _get_inventory_value(self, cr, uid, quant, context=None):
+        res = super(StockQuant, self)._get_inventory_value(
+            cr, uid, quant, context=context)
+        product_currency = quant.product_id.currency_id
+        company_currency = quant.company_id.currency_id
+        if product_currency != company_currency:
+            standard_price = product_currency.compute(
+                quant.product_id.standard_price,
+                company_currency, round=True)
+            res = standard_price * quant.qty
+            # for stock_account implementation
+            if quant.product_id.cost_method in ('real'):
+                cost = product_currency.compute(
+                    quant.cost, company_currency, round=True)
+                res = cost * quant.qty
+        return res
+
     @api.model
     def _prepare_account_move_line(
             self, move, qty, cost, credit_account_id, debit_account_id):
@@ -20,8 +37,8 @@ class StockQuant(models.Model):
             else:
                 valuation_amount = cost if move.product_id.cost_method == 'real'\
                     else move.product_id.standard_price
-            self = self.with_context(force_valuation_amount=move.product_id.
-                currency_id.compute(
+            self = self.with_context(
+                force_valuation_amount=move.product_id.currency_id.compute(
                     valuation_amount, move.company_id.currency_id, round=True))
         return super(
             StockQuant, self)._prepare_account_move_line(
