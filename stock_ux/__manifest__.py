da9098246c337b8c6699ce5f70bfd69d469897a4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-02-07 16:22:11 -0300

    [FIX] show set all done regardless of the show_details_visible
    We also fix it because the attrs condition was wrong, instaed of ('show_details_visible', '=', False) should be ('show_details_visible', '=', True)

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': '11.0.1.8.0',
+    'version': '11.0.1.9.0',
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
