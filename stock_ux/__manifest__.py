f2a0c0bff9de9bab9e58f36a391cc7edb35d94f2
nicomacr <nmr@adhoc.com.ar>
2019-04-10 10:47:28 -0300

    [FIX] stock_ux: fix in stock move line view to create a lot

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': '11.0.1.12.0',
+    'version': '11.0.1.13.0',
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
