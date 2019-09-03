5cb9c843145cbb74f9457eb29693f9a7b77d5b7a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-09-03 12:11:26 -0300

    [REF] stock_ux: increase manifest version

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': '11.0.1.19.0',
+    'version': '11.0.1.20.0',
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
