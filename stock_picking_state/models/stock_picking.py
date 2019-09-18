7d23b29f33777d9d9466fda62bc781cca498cb7b
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-09-18 16:05:49 -0300

    [PORT] from v11
    [IMP] stock_picking_state: Improve in change states (#190)
    
    IF you change the state of the picking automaticaly move the state detail of the picking to the corresponding state detail the first of all the details status for this state and type

--- stock_picking_state/models/stock_picking.py
+++ stock_picking_state/models/stock_picking.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StockPicking(models.Model):
@@ -16,3 +16,13 @@ class StockPicking(models.Model):
         index=True,
         copy=False,
     )
+
+    @api.constrains('state')
+    def change_state(self):
+        for rec in self:
+            domain = [
+                ('state', '=', rec.state),
+                ('picking_type', '=', rec.picking_type_code)]
+            state_detail = self.env['stock.picking.state_detail'].search(
+                domain, limit=1, order="sequence asc")
+            rec.state_detail_id = state_detail
