b3dd3dd407e5ad2cd15cc0912904fa2b1cf7ca0b
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-10-22 11:06:02 -0300

    [IMP] stock_ux: Add invetory valuation by stock (#195)
    Add new report on stock reports. For the stock valuation (on quants)  to see the values by location, becase odoo remove this feature since v11 and reciently incoporate them on v13.

---
+++ stock_ux/models/stock_quant.py
@@ -0,0 +1,41 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields, api
+
+
+class StockQuant(models.Model):
+    """ TODO Remove this in v13, all are included in that odoo version
+    """
+    _inherit = 'stock.quant'
+
+    value = fields.Float('Value', compute='_compute_value', readonly=True)
+
+    @api.depends('product_id.cost_method', 'quantity',
+                 'product_id.standard_price')
+    def _compute_value(self):
+        """ Compute the current accounting valuation of the quants by multiplying the quantity by
+        the standard price. This works well for standard and AVCO valuation, while not so much in
+        FIFO because it'll estimate the products at their last delivery price and not their real
+        value.
+        """
+        for quant in self:
+            quant.value = quant.quantity * quant.product_id.standard_price
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False,lazy=True):
+        """ This override is done in order for the grouped list view to display the total value of
+        the quants inside a location. This doesn't work out of the box because `value` is a computed
+        field.
+        """
+        if 'value' not in fields:
+            return super().read_group(domain, fields, groupby, offset=offset, limit=limit,
+                orderby=orderby, lazy=lazy)
+        res = super().read_group(domain, fields, groupby, offset=offset, limit=limit,
+            orderby=orderby, lazy=lazy)
+        for group in res:
+            if group.get('__domain'):
+                quants = self.search(group['__domain'])
+                group['value'] = sum(quant.value for quant in quants)
+        return res
