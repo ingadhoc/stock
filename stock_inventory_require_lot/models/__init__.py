032e255acfaa1a7f7ab3b97a4e80bb1a3f6e4cd1
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-10-29 11:08:33 -0300

    [REM] stock_inventory_require_lot: Deprecated in v12

--- stock_inventory_require_lot/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import stock_inventory_line
-from . import stock_change_product_qty
