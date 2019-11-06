926deb28c216a34417af2529d12c6ed1911b77cc
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-11-06 18:13:07 -0300

    [REM] stock_inventory_require_lot: Deprecated in v12 (#201)
    * [REM] stock_inventory_require_lot: Deprecated in v12
    
    * [FIX] stock_ux: fix lint error

--- stock_ux/models/stock_quant.py
+++ stock_ux/models/stock_quant.py
@@ -15,24 +15,30 @@ class StockQuant(models.Model):
     @api.depends('product_id.cost_method', 'quantity',
                  'product_id.standard_price')
     def _compute_value(self):
-        """ Compute the current accounting valuation of the quants by multiplying the quantity by
-        the standard price. This works well for standard and AVCO valuation, while not so much in
-        FIFO because it'll estimate the products at their last delivery price and not their real
+        """ Compute the current accounting valuation of the quants
+        by multiplying the quantity bythe standard price. This works well for
+        standard and AVCO valuation, while not so much in FIFO because it'll
+        estimate the products at their last delivery price and not their real
         value.
         """
         for quant in self:
             quant.value = quant.quantity * quant.product_id.standard_price
 
     @api.model
-    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False,lazy=True):
-        """ This override is done in order for the grouped list view to display the total value of
-        the quants inside a location. This doesn't work out of the box because `value` is a computed
+    def read_group(
+            self, domain, fields, groupby, offset=0, limit=None, orderby=False,
+            lazy=True):
+        """ This override is done in order for the grouped list view to display
+        the total value of the quants inside a location. This doesn't work out
+        of the box because `value` is a computed
         field.
         """
         if 'value' not in fields:
-            return super().read_group(domain, fields, groupby, offset=offset, limit=limit,
+            return super().read_group(
+                domain, fields, groupby, offset=offset, limit=limit,
                 orderby=orderby, lazy=lazy)
-        res = super().read_group(domain, fields, groupby, offset=offset, limit=limit,
+        res = super().read_group(
+            domain, fields, groupby, offset=offset, limit=limit,
             orderby=orderby, lazy=lazy)
         for group in res:
             if group.get('__domain'):
