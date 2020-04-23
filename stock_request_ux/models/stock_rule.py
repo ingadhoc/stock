d80ccdbb2b61c7ce7a45daa1e74cdeaccba390e5
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-04-23 15:23:38 -0300

    [FIX] change procurement.rule to stock.rule

--- stock_request_ux/models/stock_rule.py
+++ stock_request_ux/models/stock_rule.py
@@ -6,20 +6,20 @@
 from odoo import models
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
-        result = super(ProcurementRule, self)._get_stock_move_values(
+    def _get_stock_move_values(
+            self, product_id, product_qty, product_uom, location_id, name,
+            origin, company_id, values):
+        result = super()._get_stock_move_values(
             product_id, product_qty, product_uom, location_id, name, origin,
-            values, group_id)
+            company_id, values)
         stock_request_id = values.get('stock_request_id', False)
         if stock_request_id:
             # This it's because odoo clean the partner_id and origin in the
             # picking if has more than one stock request in the request order
-            # https://github.com/odoo/odoo/blob/11.0/addons/
-            # stock/models/stock_move.py#L696L703
+            # https://github.com/odoo/odoo/blob/13.0/addons/stock/models/stock_move.py#L887L896
             result['origin'] = self.env['stock.request'].browse(
                 stock_request_id).order_id.name
         return result
