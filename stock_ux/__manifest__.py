2f21aaaf22e70113175e6d9f7c2d9aba51f0b57a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-07-01 18:01:39 -0300

    [ADD] stock_ux: add observations from sale_stock_ux

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
@@ -46,6 +46,7 @@
         'views/stock_backorder_confirmation_views.xml',
         'views/stock_return_picking_views.xml',
         'views/stock_picking_type_views.xml',
+        'views/report_deliveryslip.xml',
         'wizards/stock_operation_wizard_views.xml',
         'wizards/res_config_settings_views.xml',
         'report/stock_ux_report.xml',
