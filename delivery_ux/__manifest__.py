ad1e27ffdf569dd6b49afcb88a26a04ddd7058c9
matiasperalta1 <matias_peralta1@hotmail.com>
2021-08-24 09:34:33 -0300

    [FIX] stock: delivery_ux auto-install

--- delivery_ux/__manifest__.py
+++ delivery_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Delivery UX',
-    'version': "13.0.1.0.0",
+    'version': "13.0.1.1.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
@@ -37,6 +37,6 @@
     'demo': [
     ],
     'installable': True,
-    'auto_install': False,
+    'auto_install': True,
     'application': False,
 }
