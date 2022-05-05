a6788bd24c4623cf5175f4fe362b6dd48e97cbc5
Joel Zilli <joz@adhoc.com.ar>
2022-05-05 09:21:13 -0300

    [MIG] stock_picking_ean128_report: Migration to 15.0

--- stock_picking_ean128_report/__manifest__.py
+++ stock_picking_ean128_report/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock Picking EAN128 Report',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
@@ -43,7 +43,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
