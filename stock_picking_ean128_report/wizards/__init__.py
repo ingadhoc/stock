457f2b4ed1c009c61fe60c76d80d07bc087e14f1
Joel Zilli <joz@adhoc.com.ar>
2022-05-31 13:07:23 -0300

    [REM] Removed stock_picking_ean128_report

--- stock_picking_ean128_report/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_lot_print_ean128_report
-from . import stock_picking_print_ean128_report
-from . import stock_picking_print_ean128_report_detail
