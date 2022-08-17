84d1ad7f5adfd2fe2faed5a9ef09ac5d30a2ba66
Augusto Weiss <awe@adhoc.com.ar>
2022-08-17 15:05:09 +0000

    [MIG] stock_removal_by_location_priority: Migration to 15.0
    closes ingadhoc/stock#336
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- stock_removal_by_location_priority/__manifest__.py
+++ stock_removal_by_location_priority/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Removal by Location priority',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
@@ -18,7 +18,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
     'pre_init_hook': 'pre_init_hook',
