c03650b23802aea14404ffceea93f52c46ce699d
juancarreras97 <jc@adhoc.com.ar>
2022-08-18 18:18:29 +0000

    [MIG] location_security: Migration to 15.0
    closes ingadhoc/stock#337
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- location_security/__manifest__.py
+++ location_security/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Location Security',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': '',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
@@ -32,7 +32,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
