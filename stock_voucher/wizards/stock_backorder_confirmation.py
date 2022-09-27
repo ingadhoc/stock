cf482322946e1f699a94aeadd9da547114fbb041
Virginia Bonservizi <vib@adhoc.com.ar>
2022-09-27 12:25:44 +0000

    [FIX] stock_voucher: adjustments to validate more than one picking
    closes ingadhoc/stock#340
    
    Signed-off-by: Augusto Weiss <awe@adhoc.com.ar>

--- stock_voucher/wizards/stock_backorder_confirmation.py
+++ stock_voucher/wizards/stock_backorder_confirmation.py
@@ -13,34 +13,34 @@ class StockBackorderConfirmation(models.TransientModel):
 
     def process(self):
         super().process()
-        picking = self.env['stock.picking'].browse(
+        pickings = self.env['stock.picking'].browse(
             # if we came, for eg, from a sale order, active_id would be the
             # sale order id
             # self._context.get('active_id'))
             # TODO we should also fix odoo methods
-            self._context.get('picking_id'))
-        if picking.book_required:
+            self._context.get('picking_ids')).filtered('book_required')
+        if pickings:
             return {
                 'actions': [
                     {'type': 'ir.actions.act_window_close'},
-                    picking.do_print_voucher(),
+                    pickings.do_print_voucher(),
                 ],
                 'type': 'ir.actions.act_multi',
             }
 
     def process_cancel_backorder(self):
         super().process_cancel_backorder()
-        picking = self.env['stock.picking'].browse(
+        pickings = self.env['stock.picking'].browse(
             # if we came, for eg, from a sale order, active_id would be the
             # sale order id
             # self._context.get('active_id'))
             # TODO we should also fix odoo methods
-            self._context.get('picking_id'))
-        if picking.book_required:
+            self._context.get('picking_ids')).filtered('book_required')
+        if pickings:
             return {
                 'actions': [
                     {'type': 'ir.actions.act_window_close'},
-                    picking.do_print_voucher(),
+                    pickings.do_print_voucher(),
                 ],
                 'type': 'ir.actions.act_multi',
             }
--- stock_voucher/wizards/stock_immediate_transfer.py
+++ stock_voucher/wizards/stock_immediate_transfer.py
@@ -10,22 +10,22 @@ class StockImmediateTransfer(models.TransientModel):
 
     def process(self):
         res = super().process()
-        picking = self.env['stock.picking'].browse(
+        pickings = self.env['stock.picking'].browse(
             # if we came, for eg, from a sale order, active_id would be the
             # sale order id
             # self._context.get('active_id'))
             # TODO we should also fix odoo methods
-            self._context.get('picking_id'))
+            self._context.get('picking_ids')).filtered(lambda p: p.book_required and p.voucher_ids)
         # solo viene con vouchers en entrega total, sin backorder
         # si hay backorder se termina imprimiendo desde el backorder
         # confirmation
-        if picking.book_required and picking.voucher_ids:
+        if pickings:
             if isinstance(res, bool):
                 res = {'type': 'ir.actions.act_window_close'}
             return {
                 'actions': [
                     res,
-                    picking.do_print_voucher(),
+                    pickings.do_print_voucher(),
                 ],
                 'type': 'ir.actions.act_multi',
             }
