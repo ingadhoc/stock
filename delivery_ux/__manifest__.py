85fc40dfff437402fc9d6ccdb2e450c11474e538
augusto-weiss <awe@adhoc.com.ar>
2023-04-11 14:20:18 +0000

    [UPD] delivery_ux: deprecate zip_codes field, Odoo included zip_prefix_ids
    closes ingadhoc/stock#367
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- delivery_ux/__manifest__.py
+++ delivery_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Delivery UX',
-    'version': "16.0.1.0.0",
+    'version': "16.0.1.1.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
--- delivery_ux/models/delivery_carrier.py
+++ delivery_ux/models/delivery_carrier.py
@@ -11,22 +11,3 @@ class DeliveryCarrier(models.Model):
         string='Carrier Address',
         help="Utilizado para el remito."
     )
-    zip_codes = fields.Char(
-        string="Zip codes",
-        help="A list of zip codes availables for this shipping method.\n"
-             "If the zip code of the client is not in this list it will not be able to select it."
-    )
-
-    @api.constrains('zip_codes')
-    def check_zip_codes_list(self):
-        try:
-            if self.zip_codes and not isinstance(safe_eval(self.zip_codes), list):
-                raise Exception
-        except Exception:
-            raise ValidationError(_("Invalid expression, the zip codes must be a list of numbers, something like: ['code1', 'code2']"))
-
-    def _match_address(self, partner):
-        res = super()._match_address(partner)
-        if res and self.zip_codes and partner.zip not in safe_eval(self.zip_codes):
-            return False
-        return res
