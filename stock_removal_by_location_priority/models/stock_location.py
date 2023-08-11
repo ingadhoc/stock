9dbc62532fb217a2dfd1580ba85b2a811b0d3a51
Virginia Bonservizi <vib@adhoc.com.ar>
2023-08-11 12:40:39 +0000

    [IMP] stock_removal_by_location_priority: refactor?
    closes ingadhoc/stock#392
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- stock_removal_by_location_priority/models/stock_location.py
+++ stock_removal_by_location_priority/models/stock_location.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
+    removal_strategy_method = fields.Char(related='removal_strategy_id.method')
     removal_priority = fields.Integer(
         default=10,
         help='Priority used on stock removal if "Force removal by location "'
