5c3d7ad63ccdba1e1059cc335872d9d9f90b348c
mav-adhoc <mav@adhoc.com.ar>
2023-08-30 19:25:54 +0000

    [FIX]stock_ux:access to picking.line with a SO associated
    closes ingadhoc/stock#397
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': "16.0.2.7.0",
+    'version': "16.0.2.8.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
