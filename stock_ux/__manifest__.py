e5283face4c44f26b6d62ea529d0bef419b1c5b7
Juan Ignacio Carreras <jc@adhoc.com.ar>
2023-11-28 10:30:27 -0300

    [FIX]stock_ux:manifest
    X-original-commit: e720cecef316a8e0db922e082a5808517eff07d4

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': "16.0.2.11.0",
+    'version': "16.0.2.12.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
