54ab5b5c9ef48f68169ea4772169ff02f6ba9371
Bruno Zanotti <bz@adhoc.com.ar>
2024-01-19 14:38:35 +0000

    [REM] stock_orderpoint_manual_update: remove wizard
    Part-of: ingadhoc/stock#427

--- stock_orderpoint_manual_update/models/stock_orderpoint.py
+++ stock_orderpoint_manual_update/models/stock_orderpoint.py
@@ -14,27 +14,6 @@ class StockWarehouseOrderpoint(models.Model):
         self.update_qty_forecast()
         return action
 
-    def _get_orderpoint_products(self):
-        domain = [('type', '=', 'product'), ('stock_move_ids', '!=', False)]
-
-        # Filter by suppliers
-        suppliers_ids = self._context.get('suppliers')
-        suppliers = self.env['product.supplierinfo'].browse(suppliers_ids)
-        if suppliers:
-            domain += ['|', ('product_tmpl_id', 'in', suppliers.product_tmpl_id.ids), ('id', 'in', suppliers.product_id.ids)]
-
-        # Filter by product categories
-        category_ids = self._context.get('categories')
-        if category_ids:
-            domain += [('categ_id', 'in', category_ids)]
-
-        # Filter by products
-        product_ids = self._context.get('products')
-        if product_ids:
-            domain += [('id', 'in', product_ids)]
-
-        return self.env['product.product'].search(domain)
-
     def update_qty_forecast(self):
         orderpoints = self.with_context(active_test=False).search([])
         for rec in orderpoints:
