f17d4e3d7ddb634d0581a7d14c23290878c2aa7e
Bruno Zanotti <bz@adhoc.com.ar>
2024-03-13 18:59:45 -0300

    [NO MERGE] Revert "[FIX] stock: remove depends qty_forecast"

--- stock_ux/models/stock_warehouse_orderpoint.py
+++ stock_ux/models/stock_warehouse_orderpoint.py
@@ -63,3 +63,11 @@ class StockWarehouseOrderpoint(models.Model):
                 'warehouse_rotation_stdev': warehouse_rotation_stdev,
                 'warehouse_rotation': warehouse_rotation,
             })
+
+    @api.model
+    def action_open_orderpoints(self):
+        return self._get_orderpoint_action()
+
+    @api.depends('qty_multiple', 'qty_forecast', 'product_min_qty', 'product_max_qty', 'visibility_days')
+    def _compute_qty_to_order(self):
+        return super()._compute_qty_to_order()
