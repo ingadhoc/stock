acc1a8fa8911e28fd5bbbd340ed9dd1026f444d3
Bruno Zanotti <bz@adhoc.com.ar>
2024-03-26 22:20:45 +0000

    [IMP] stock_ux: inactive lines warning decorator
    Part-of: ingadhoc/stock#467

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': "16.0.2.20.0",
+    'version': "16.0.2.21.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
