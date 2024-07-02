969ae4cb56b227bf3065440fa3c65fcd52d31ae8
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-07-02 17:55:15 +0000

    [MIG] stock_removal_by_location_priority: Migration to 17.0
    closes ingadhoc/stock#501
    
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- stock_removal_by_location_priority/init_hook.py
+++ stock_removal_by_location_priority/init_hook.py
@@ -6,7 +6,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     """
     The objective of this hook is to speed up the installation
     of the module on an existing Odoo instance.
@@ -14,18 +14,18 @@ def pre_init_hook(cr):
     Without this script, big databases can take a long time to install this
     module.
     """
-    set_stock_location_removal_priority_default(cr)
-    set_stock_quant_removal_priority_default(cr)
+    set_stock_location_removal_priority_default(env)
+    set_stock_quant_removal_priority_default(env)
 
 
-def set_stock_location_removal_priority_default(cr):
-    cr.execute("""SELECT column_name
+def set_stock_location_removal_priority_default(env):
+    env.cr.execute("""SELECT column_name
     FROM information_schema.columns
     WHERE table_name='stock_location' AND
     column_name='removal_priority'""")
-    if not cr.fetchone():
+    if not env.cr.fetchone():
         logger.info('Creating field removal_priority on stock_location')
-        cr.execute(
+        env.cr.execute(
             """
             ALTER TABLE stock_location
             ADD COLUMN removal_priority integer
@@ -33,14 +33,14 @@ def set_stock_location_removal_priority_default(cr):
             """)
 
 
-def set_stock_quant_removal_priority_default(cr):
-    cr.execute("""SELECT column_name
+def set_stock_quant_removal_priority_default(env):
+    env.cr.execute("""SELECT column_name
     FROM information_schema.columns
     WHERE table_name='stock_quant' AND
     column_name='removal_priority'""")
-    if not cr.fetchone():
+    if not env.cr.fetchone():
         logger.info('Creating field removal_priority on stock_quant')
-        cr.execute(
+        env.cr.execute(
             """
             ALTER TABLE stock_quant
             ADD COLUMN removal_priority integer
