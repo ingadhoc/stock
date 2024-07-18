7f7b69593b1acb3124aa84279d4d2ade04b9b096
Bruno Zanotti <bz@adhoc.com.ar>
2024-07-18 19:50:43 -0300

    [IMP] stock_ux: inherit catalog tree view

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock UX',
-    'version': "17.0.1.4.0",
+    'version': "17.0.1.5.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
