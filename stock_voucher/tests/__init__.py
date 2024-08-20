36b18022885c5e95d1e08b6c489788dd37703c17
Celina Devigili <ced@adhoc.com.ar>
2024-08-20 18:21:43 +0000

    [IMP] stock_voucher: unit tests in v17
    closes ingadhoc/stock#523
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

---
+++ stock_voucher/tests/__init__.py
@@ -0,0 +1,6 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import test_stock_picking_voucher
+from . import test_stock_book
