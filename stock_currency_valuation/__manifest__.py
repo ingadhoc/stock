f56d957199a398fb1c32067f6ab509ca5e08901d
Martin Quinteros <maq@adhoc.com.ar>
2024-09-18 12:24:58 -0300

    [IMP] Add new module stock_currency_valuation module
    The new stock_currency_valuation module allows you to add a secondary currency in the product categories to perform the valuation in real time in that currency.
    
    It also allows you to adjust the exchange rate of this currency when loading a picking and a landed cost. In invoices, it only represents the exchange difference in the company's currency, it does not modify the valuation in the secondary currency.

---
+++ stock_currency_valuation/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Stock currency valuation',
+    'version': "16.0.1.0.0",
+    'category': 'Warehouse Management',
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'images': [
+    ],
+    'depends': [
+        'stock_account',
+        'stock_landed_costs',
+        'product_replenishment_cost',
+    ],
+    'data': [
+        'views/product_category.xml',
+        'views/stock_picking.xml',
+        'views/stock_landed_cost_views.xml',
+        'views/product.xml',
+        'views/stock_valuation_layer.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+    },
+    'license': 'AGPL-3',
+}
---
+++ stock_currency_valuation/models/stock_picking.py
@@ -0,0 +1,39 @@
+from odoo import models, fields, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    valuation_currency_id = fields.Many2one(
+        'res.currency',
+        string='Secondary Currency Valuation',
+        compute='_compute_valuation_currency_id',
+    )
+    inverse_currency_rate = fields.Float(
+        string="Cotizacion",
+        compute='_compute_inverse_currency_rate',
+        inverse='_inverse_currency_rate',
+        help='If no rate is defined, the rate of the confirmation date is used.',
+    )
+    currency_rate = fields.Float(
+        digits=0,
+        copy=False,
+        help="If no rate is defined, the rate of the confirmation date is used.",
+    )
+
+    @api.depends('currency_rate')
+    def _compute_inverse_currency_rate(self):
+        for rec in self:
+            rec.inverse_currency_rate = 1/rec.currency_rate if rec.currency_rate else 0
+
+    def _inverse_currency_rate(self):
+        for rec in self:
+            rec.currency_rate = 1/rec.inverse_currency_rate if rec.inverse_currency_rate else 0
+
+    def _compute_valuation_currency_id(self):        
+        for rec in self.filtered(lambda x: x.purchase_id and x.picking_type_id.code == 'incoming' ):
+            valuation_currency_id = rec.move_ids.with_company(rec.company_id.id).mapped('product_id.categ_id.valuation_currency_id')
+            if len(valuation_currency_id) == 1:
+                self = self - rec
+                rec.valuation_currency_id = valuation_currency_id.id    
+        self.valuation_currency_id = False
