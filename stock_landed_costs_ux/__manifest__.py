b1bbb0f9735ca0c2547d2f9740b1c0530ae0d681
docker-odoo <docker-odoo@example.com>
2024-12-10 19:58:00 +0000

    [MIG] stock_landed_costs_ux: Migration to 18.0

--- stock_landed_costs_ux/__manifest__.py
+++ stock_landed_costs_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Stock Landed Costs UX',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Warehouse Management',
     'sequence': 14,
     'summary': '',
@@ -33,7 +33,7 @@
     ],
     'data': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
     'license': 'AGPL-3',
