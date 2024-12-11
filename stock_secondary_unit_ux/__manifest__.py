eabaab3907762ef3c3cba3fbed508e89225b0ba0
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-12-11 17:05:04 -0300

    [ADD]stock_secondary_unit_ux: new module

---
+++ stock_secondary_unit_ux/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock secondary unit ux",
+    "version": "16.0.1.0.0",
+    "author": "adhocsa",
+    "website": "https://github.com/OCA/stock-logistics-availability",
+    "development_status": "Production/Stable",
+    "category": "Warehouse",
+    "depends": ["stock","stock_secondary_unit","stock_ux"],
+    "license": "AGPL-3",
+    "data": [
+        "report/report_deliveryslip.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
