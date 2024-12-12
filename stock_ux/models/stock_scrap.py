16735d1183937381fd188ddcf8547d53841b1581
mav-adhoc <mav@adhoc.com.ar>
2024-12-12 12:05:42 +0000

    [FIX]stock_ux: block_additional_qty for scrap
    closes ingadhoc/stock#602
    
    X-original-commit: 8aadf542d8ce6fed8533108d3068de1a926ac2ea
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

---
+++ stock_ux/models/stock_scrap.py
@@ -0,0 +1,21 @@
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+class StockScrap(models.Model):
+    _inherit = 'stock.scrap'
+
+    def action_validate(self):
+        total_scraped = sum(scrap.scrap_qty for scrap in self.env['stock.scrap'].search(
+            [('product_id', '=', self.product_id.id),
+            ('picking_id', '=', self.picking_id.id),
+            ('state', '=', 'done')]
+        ))
+        total_picking = sum(
+            move_id.product_uom_qty
+            for move_id in self.picking_id.move_ids
+            if move_id.product_id == self.product_id
+        )
+        if (self.scrap_qty + total_scraped) > total_picking:
+                raise UserError(_('No se puede desechar una cantidad mayor a la cantidad dentro de la transferencia del producto: %s.') % self.product_id.display_name)
+
+        return super().action_validate()
