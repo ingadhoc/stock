29b817c4d4d0ea7adc18a36c4c2a580287c26583
mav-adhoc <mav@adhoc.com.ar>
2025-01-30 13:31:28 -0300

    [FIX]stock_ux: scrap restriction for scrap without picking

--- stock_ux/models/stock_scrap.py
+++ stock_ux/models/stock_scrap.py
@@ -15,7 +15,7 @@ class StockScrap(models.Model):
             for move_id in self.picking_id.move_ids
             if move_id.product_id == self.product_id
         )
-        if (self.scrap_qty + total_scraped) > total_picking:
+        if self.picking_id and (self.scrap_qty + total_scraped) > total_picking:
                 raise UserError(_('No se puede desechar una cantidad mayor a la cantidad dentro de la transferencia del producto: %s.') % self.product_id.display_name)
 
         return super().action_validate()
