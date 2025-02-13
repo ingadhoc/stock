ee9928e73a87cdf7644462b094d67605962a64ee
mav-adhoc <mav@adhoc.com.ar>
2025-02-13 17:49:28 +0000

    [FIX]stock_batch_picking_voucher: stock_voucher_ux dependencie
    closes ingadhoc/stock#643
    
    closes ingadhoc/stock#649
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

---
+++ stock_batch_picking_ux/wizard/stock_picking_to_batch.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class StockPickingToBatch(models.TransientModel):
+    _inherit = 'stock.picking.to.batch'
+
+    def attach_pickings(self):
+        res = super().attach_pickings()
+        pickings = self.env['stock.picking'].browse(self.env.context.get('active_ids'))
+        partner_id = pickings[0].partner_id if pickings else None
+        all_same_partner = all(picking.partner_id == partner_id for picking in pickings)
+        if all_same_partner:
+           batch = pickings.batch_id
+           batch.partner_id = partner_id
