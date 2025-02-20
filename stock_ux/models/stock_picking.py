99ef83b383a5d68ddd3a3e418c9770d510c4435c
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-02-20 12:20:08 +0000

    [FIX]stock_ux:template in operation type
    closes ingadhoc/stock#648
    
    X-original-commit: f03cf422637b9d992fd443cfbc2ffc1a02fd7857
    Signed-off-by: ced-adhoc <ced@adhoc.com.ar>
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- stock_ux/models/stock_picking.py
+++ stock_ux/models/stock_picking.py
@@ -72,24 +72,22 @@ class StockPicking(models.Model):
             self.move_ids.update(
                 {'location_dest_id': self.location_dest_id.id})
 
-    def _send_confirmation_email(self):
-        for rec in self:
-            if rec.picking_type_id.mail_template_id:
-                try:
-                    rec.message_post_with_source(rec.picking_type_id.mail_template_id)
-                except Exception as error:
-                    title = _(
-                        "ERROR: Picking was not sent via email"
-                    )
-                    rec.message_post(body="<br/><br/>".join([
-                        "<b>" + title + "</b>",
-                        _("Please check the email template associated with"
-                            " the picking type."),
-                        "<code>" + str(error) + "</code>"
-                    ]), body_is_html=True
-                    )
-            else:
-                super(StockPicking, self)._send_confirmation_email()
+    def _action_done(self):
+        for rec in self.with_context(mail_notify_force_send=False).filtered('picking_type_id.mail_template_id'):
+            try:
+                rec.message_post_with_template(rec.picking_type_id.mail_template_id.id)
+            except Exception as error:
+                title = _(
+                    "ERROR: Picking was not sent via email"
+                )
+                rec.message_post(body="<br/><br/>".join([
+                    "<b>" + title + "</b>",
+                    _("Please check the email template associated with"
+                      " the picking type."),
+                    "<code>" + str(error) + "</code>"
+                ]),
+                )
+        return super()._action_done()
 
     def new_force_availability(self):
         self.action_assign()
