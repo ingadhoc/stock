179200beb608cb4d616195cdcd67fd34e60a058e
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-03-20 14:42:15 -0300

    [FIX]stock_ux: orderpoint qties

--- stock_ux/models/stock_picking.py
+++ stock_ux/models/stock_picking.py
@@ -8,14 +8,12 @@ from odoo.exceptions import ValidationError, UserError
 
 
 class StockPicking(models.Model):
-
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     block_manual_lines = fields.Boolean(
-        related='picking_type_id.block_manual_lines',
+        related="picking_type_id.block_manual_lines",
     )
-    observations = fields.Html(
-    )
+    observations = fields.Html()
 
     def unlink(self):
         """
@@ -23,17 +21,18 @@ class StockPicking(models.Model):
         draft or cancel
         """
         not_del_pickings = self.filtered(
-            lambda x: x.picking_type_id.block_picking_deletion or x.state
-            not in ('draft', 'cancel'))
+            lambda x: x.picking_type_id.block_picking_deletion or x.state not in ("draft", "cancel")
+        )
         if not_del_pickings:
             raise ValidationError(
                 _(
                     'You can not delete this pickings because "Block picking '
                     'deletion" is enable on the picking type/s "%s" '
-                    'or the state of the picking is not draft or cancel.\n'
-                    'Picking Ids: %s') %
-                (','.join(not_del_pickings.mapped('picking_type_id.name')),
-                 not_del_pickings.ids))
+                    "or the state of the picking is not draft or cancel.\n"
+                    "Picking Ids: %s"
+                )
+                % (",".join(not_del_pickings.mapped("picking_type_id.name")), not_del_pickings.ids)
+            )
         return super().unlink()
 
     def copy(self, default=None):
@@ -42,17 +41,20 @@ class StockPicking(models.Model):
         # si tiene bloqueado agregar cantidades entonces
         # tiene bloqueado duplicar
         if not default and self.picking_type_id.block_additional_quantity:
-            raise UserError(_(
-                'You can not duplicate a Picking because "Block'
-                ' Additional Quantity"'
-                ' is enable on the picking type "%s"') % (
-                self.picking_type_id.name))
+            raise UserError(
+                _(
+                    'You can not duplicate a Picking because "Block'
+                    ' Additional Quantity"'
+                    ' is enable on the picking type "%s"'
+                )
+                % (self.picking_type_id.name)
+            )
         return super().copy(default=default)
 
     def add_picking_operation(self):
         self.ensure_one()
-        view_id = self.env.ref('stock_ux.view_move_line_tree').id
-        search_view_id = self.env.ref('stock_ux.stock_move_line_view_search').id
+        view_id = self.env.ref("stock_ux.view_move_line_tree").id
+        search_view_id = self.env.ref("stock_ux.stock_move_line_view_search").id
         return {
             "type": "ir.actions.act_window",
             "res_model": "stock.move.line",
@@ -62,39 +64,38 @@ class StockPicking(models.Model):
             "context": {"create": False},
         }
 
-    @api.onchange('location_id')
+    @api.onchange("location_id")
     def change_location(self):
         # we only change moves locations if picking in draft
-        if self.state == 'draft':
-            self.move_ids.update({'location_id': self.location_id.id})
+        if self.state == "draft":
+            self.move_ids.update({"location_id": self.location_id.id})
 
-    @api.onchange('location_dest_id')
+    @api.onchange("location_dest_id")
     def change_location_dest(self):
         # we only change moves locations if picking in draft
-        if self.state == 'draft':
-            self.move_ids.update(
-                {'location_dest_id': self.location_dest_id.id})
+        if self.state == "draft":
+            self.move_ids.update({"location_dest_id": self.location_dest_id.id})
 
     def _action_done(self):
-        for rec in self.with_context(mail_notify_force_send=False).filtered('picking_type_id.mail_template_id'):
+        for rec in self.with_context(mail_notify_force_send=False).filtered("picking_type_id.mail_template_id"):
             try:
                 rec.message_post_with_template(rec.picking_type_id.mail_template_id.id)
             except Exception as error:
-                title = _(
-                    "ERROR: Picking was not sent via email"
-                )
-                rec.message_post(body="<br/><br/>".join([
-                    "<b>" + title + "</b>",
-                    _("Please check the email template associated with"
-                      " the picking type."),
-                    "<code>" + str(error) + "</code>"
-                ]),
+                title = _("ERROR: Picking was not sent via email")
+                rec.message_post(
+                    body="<br/><br/>".join(
+                        [
+                            "<b>" + title + "</b>",
+                            _("Please check the email template associated with the picking type."),
+                            "<code>" + str(error) + "</code>",
+                        ]
+                    ),
                 )
         return super()._action_done()
 
     def new_force_availability(self):
         self.action_assign()
-        for rec in self.mapped('move_ids').filtered(lambda m: m.state not in ['cancel', 'done']):
+        for rec in self.mapped("move_ids").filtered(lambda m: m.state not in ["cancel", "done"]):
             # this two could go together but we keep similar to odoo sm._quantity_done_set
             if not rec.move_line_ids:
                 rec.quantity_done = rec.product_uom_qty
--- stock_ux/models/stock_warehouse_orderpoint.py
+++ stock_ux/models/stock_warehouse_orderpoint.py
@@ -3,40 +3,38 @@
 # directory
 ##############################################################################
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.osv import expression
 
 
 class StockWarehouseOrderpoint(models.Model):
-    _name = 'stock.warehouse.orderpoint'
-    _inherit = ['stock.warehouse.orderpoint', 'mail.thread']
+    _name = "stock.warehouse.orderpoint"
+    _inherit = ["stock.warehouse.orderpoint", "mail.thread"]
 
-    active_product = fields.Boolean(string="Product Active", related='product_id.active')
+    active_product = fields.Boolean(string="Product Active", related="product_id.active")
     rotation_stdev = fields.Float(
-        compute='_compute_rotation',
-        help="Desvío estandar de las cantidades entregas a clientes en los "
-        "últimos 120 días.",
-        digits='Product Unit of Measure',
+        compute="_compute_rotation",
+        help="Desvío estandar de las cantidades entregas a clientes en los últimos 120 días.",
+        digits="Product Unit of Measure",
     )
     warehouse_rotation_stdev = fields.Float(
-        compute='_compute_rotation',
-        help="Desvío estandar de las cantidades entregas desde este almacen"
-        " a clientes en los últimos 120 días.",
-        digits='Product Unit of Measure',
+        compute="_compute_rotation",
+        help="Desvío estandar de las cantidades entregas desde este almacen a clientes en los últimos 120 días.",
+        digits="Product Unit of Measure",
     )
     rotation = fields.Float(
-        help='Cantidades entregadas a clientes en los '
-        'últimos 120 días dividido por 4 para mensualizar '
-        '(restadas devoluciones).',
-        compute='_compute_rotation',
-        digits='Product Unit of Measure',
+        help="Cantidades entregadas a clientes en los "
+        "últimos 120 días dividido por 4 para mensualizar "
+        "(restadas devoluciones).",
+        compute="_compute_rotation",
+        digits="Product Unit of Measure",
     )
     warehouse_rotation = fields.Float(
-        help='Cantidades entregadas desde este almacen a clientes en los '
-        'últimos 120 días dividido por 4 para mensualizar'
-        '(restadas devoluciones).',
-        compute='_compute_rotation',
-        digits='Product Unit of Measure',
+        help="Cantidades entregadas desde este almacen a clientes en los "
+        "últimos 120 días dividido por 4 para mensualizar"
+        "(restadas devoluciones).",
+        compute="_compute_rotation",
+        digits="Product Unit of Measure",
     )
     product_min_qty = fields.Float(tracking=True)
     product_max_qty = fields.Float(tracking=True)
@@ -44,48 +42,57 @@ class StockWarehouseOrderpoint(models.Model):
     location_id = fields.Many2one(tracking=True)
     product_id = fields.Many2one(tracking=True)
 
-    @api.depends('product_id', 'location_id')
+    @api.depends("product_id", "location_id")
     def _compute_rotation(self):
-        warehouse_with_products = self.filtered('product_id')
-        (self - warehouse_with_products).update({
-            'rotation': 0.0,
-            'rotation_stdev': 0.0,
-            'warehouse_rotation_stdev': 0.0,
-            'warehouse_rotation': 0.0,
-        })
+        warehouse_with_products = self.filtered("product_id")
+        (self - warehouse_with_products).update(
+            {
+                "rotation": 0.0,
+                "rotation_stdev": 0.0,
+                "warehouse_rotation_stdev": 0.0,
+                "warehouse_rotation": 0.0,
+            }
+        )
         for rec in warehouse_with_products:
-            rotation, rotation_stdev = rec.product_id.get_product_rotation(
-                compute_stdev=True)
-            warehouse_rotation, warehouse_rotation_stdev = \
-                rec.product_id.get_product_rotation(
-                    rec.warehouse_id.view_location_id, compute_stdev=True)
-            rec.update({
-                'rotation': rotation,
-                'rotation_stdev': rotation_stdev,
-                'warehouse_rotation_stdev': warehouse_rotation_stdev,
-                'warehouse_rotation': warehouse_rotation,
-            })
+            rotation, rotation_stdev = rec.product_id.get_product_rotation(compute_stdev=True)
+            warehouse_rotation, warehouse_rotation_stdev = rec.product_id.get_product_rotation(
+                rec.warehouse_id.view_location_id, compute_stdev=True
+            )
+            rec.update(
+                {
+                    "rotation": rotation,
+                    "rotation_stdev": rotation_stdev,
+                    "warehouse_rotation_stdev": warehouse_rotation_stdev,
+                    "warehouse_rotation": warehouse_rotation,
+                }
+            )
 
     def write(self, vals):
-        """ When archive a replenishment rule set min, max and multiple quantities in 0.
-        """
-        if 'active' in vals and not vals['active']:
-            self.write({
-                'product_min_qty': 0.0,
-                'product_max_qty': 0.0,
-                'qty_multiple': 0.0,
-            })
+        """When archive a replenishment rule set min, max and multiple quantities in 0."""
+        if "active" in vals and not vals["active"]:
+            self.write(
+                {
+                    "product_min_qty": 0.0,
+                    "product_max_qty": 0.0,
+                    "qty_multiple": 0.0,
+                }
+            )
         return super().write(vals)
 
     def _get_orderpoint_action(self):
         action = super()._get_orderpoint_action()
-        action['context'] = {
-            **action['context'],
-            'active_test': False,
+        action["context"] = {
+            **action["context"],
+            "active_test": False,
         }
-        action['domain'] = expression.AND([
-            action.get('domain', '[]'),
-            [('active_product', '=', True)],
-        ])
+        action["domain"] = expression.AND(
+            [
+                action.get("domain", "[]"),
+                [("active_product", "=", True)],
+            ]
+        )
         return action
 
+    def update_qty_to_order(self):
+        # Redefinimos ya que el metodo _compute_qty_to_order es privado
+        self._compute_qty_to_order()
