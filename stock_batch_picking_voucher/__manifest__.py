9b0730e605bde3e438c6de1f13acb29e87a3fd74
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-04-24 19:37:37 +0000

    [FIX]stock_batch_picking_ux:tree view
    closes ingadhoc/stock#697
    
    X-original-commit: 05dd837a7f74d805173f6b4664168d38e12e4f54
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- stock_batch_picking_voucher/__manifest__.py
+++ stock_batch_picking_voucher/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Preprinted report in batch pickings",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "category": "Warehouse Management",
     "sequence": 14,
     "summary": "",
