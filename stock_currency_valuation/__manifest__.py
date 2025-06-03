e979fd80a533c321082aa53bc431024bbfc63529
Martin Quinteros <maq@adhoc.com.ar>
2025-06-03 17:56:26 +0000

    [FIX] stock_currency_valuation: Fix _get_currency_price_unit when stock move is created from purchase order
    X-original-commit: e2ad5322c2d67dee05d58479148a9b32fbd538bb

---
+++ stock_currency_valuation/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Stock currency valuation',
+    'version': "16.0.2.0.0",
+    'category': 'Warehouse Management',
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'images': [
+    ],
+    'depends': [
+        'stock_account',
+        'stock_landed_costs',
+        'product_replenishment_cost',
+    ],
+    'data': [
+        'views/product_category.xml',
+        'views/stock_picking.xml',
+        'views/stock_landed_cost_views.xml',
+        'views/product.xml',
+        'views/stock_valuation_layer.xml',
+        'wizard/stock_valuation_layer_revaluation_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'assets': {
+    },
+    'license': 'AGPL-3',
+}
