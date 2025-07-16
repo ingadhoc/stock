d5c4a125005fb3b17d92db52fb2a0836e1df4d01
mav-adhoc <mav@adhoc.com.ar>
2025-07-16 18:13:47 +0000

    [FIX]stock_ux: block_add_qty when delivered in 2/3 steps
    closes ingadhoc/stock#743
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- stock_ux/models/stock_move_line.py
+++ stock_ux/models/stock_move_line.py
@@ -75,6 +75,10 @@ class StockMoveLine(models.Model):
             and not self.env.context.get("trigger_assign")
             and not self.env.context.get("from_inverse_qty_done")
             and not self.env.context.get("sale_automation")
+            and (
+                self.picking_id in self.env.context.get("picking_ids", [])
+                or not self.env.context.get("picking_ids", [])
+            )
         ):
             locations = self.env["stock.location"].search(
                 [("id", "child_of", self.picking_id.location_id.id), ("company_id", "=", self.picking_id.company_id.id)]
