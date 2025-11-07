50d254b173aa8889ab64b9a17f082cf2a0ff7b3f
Franco Leyes <lef@adhoc.com.ar>
2025-11-07 16:48:34 -0300

    [MIG] stock_landed_costs_ux: Migration to 19.0

--- stock_landed_costs_ux/__manifest__.py
+++ stock_landed_costs_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Stock Landed Costs UX",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Warehouse Management",
     "sequence": 14,
     "summary": "",
@@ -31,7 +31,7 @@
         "l10n_ar_ux",
     ],
     "data": [],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     "application": False,
     "license": "AGPL-3",
