3122fe1f517a5db88588256db337b84b23614855
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-11-21 12:12:58 +0000

    [REM]stock_voucher and stock_voucher_ux modules

--- stock_voucher/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_book
-from . import stock_picking_type
-from . import stock_picking
-from . import stock_picking_voucher
-from . import stock_move
--- stock_voucher_ux/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_book
-from . import stock_picking
