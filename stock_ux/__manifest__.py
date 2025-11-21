81bf8bb7200aec6b3747d74131867f3d687f52ff
mav-adhoc <mav@adhoc.com.ar>
2025-11-21 13:48:52 +0000

    [FIX]stock_ux: margin in zpl report
    closes ingadhoc/stock#849
    
    X-original-commit: a0ab091cc74e3918a0c79f491bf5ac3bf0f805b9
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- stock_ux/__manifest__.py
+++ stock_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Stock UX",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "category": "Warehouse Management",
     "sequence": 14,
     "summary": "",
