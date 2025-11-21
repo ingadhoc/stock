220e9b5a571fa41e3ee18dbf56c5e76e5c613ea8
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-11-21 12:12:58 +0000

    [REM]stock_voucher and stock_voucher_ux modules
    closes ingadhoc/stock#813
    
    Signed-off-by: Matias Velazquez <mav@adhoc.com.ar>

--- stock_voucher/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import test_stock_picking_voucher
-from . import test_stock_book
--- stock_voucher/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import stock_print_stock_voucher
-from . import stock_backorder_confirmation
-from . import stock_picking_wizard
