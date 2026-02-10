d39226f6ff90aefdb32cef152e4c0632fd1588c9
Felipe Garcia Suez <feg@adhoc.com.ar>
2026-02-10 15:43:58 -0300

    [FIX] stock_currency_valuation: Fix on posting moves with different company_id
    closes ingadhoc/stock#872
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- stock_currency_valuation/models/account_move.py
+++ stock_currency_valuation/models/account_move.py
@@ -1,19 +1,23 @@
-from collections import defaultdict
-
-from odoo import models, api, fields
-from odoo.tools.float_utils import float_compare, float_is_zero
+from odoo import models, api
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _post(self, soft=True):
-        income_currency_exchange_account_id = self.company_id.income_currency_exchange_account_id.id
-        return super(AccountMove, self.with_context(
-            bypass_update_product_price=True,
-            force_input_acount=income_currency_exchange_account_id,
-            default_bypass_currency_valuation=True)
-        )._post(soft=soft)
+        # Agrupar los movimientos por compañía y procesar cada grupo con su contexto
+        posted_moves = self.env['account.move']
+        for company in self.mapped('company_id'):
+            moves = self.filtered(lambda m: m.company_id == company)
+            income_currency_exchange_account_id = company.income_currency_exchange_account_id.id
+            # Llamar al super con el contexto específico de cada compañía
+            posted_moves |= super(AccountMove, moves.with_context(
+                bypass_update_product_price=True,
+                force_input_acount=income_currency_exchange_account_id,
+                default_bypass_currency_valuation=True)
+            )._post(soft=soft)
+        
+        return posted_moves
 
     @api.model_create_multi
     def create(self, vals_list):
