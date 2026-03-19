25c17c5438c4d039d8dc1c0988d37b345e66be2f
Martin Quinteros <maq@adhoc.com.ar>
2026-03-19 15:37:01 +0000

    [FIX] stock_currency_valuation: Fix ignore _prepare_pdiff_vals when svl_vals_list is none
    X-original-commit: 9f7ab12d19ac23fde70446e9972b5a2a6af6e385

---
+++ stock_currency_valuation/models/account_move_line.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+from odoo import models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def _prepare_pdiff_vals(self, layer, aml, layer_price_unit, out_qty_to_invoice, qty_to_correct):
+        svl_vals_list, aml_vals_list = super()._prepare_pdiff_vals(
+            layer, aml, layer_price_unit, out_qty_to_invoice, qty_to_correct
+        )
+        valuation_currency_id = self.product_id.with_company(self.company_id.id).categ_id.valuation_currency_id
+        use_valuation_currency = valuation_currency_id == self.currency_id == self.purchase_line_id.currency_id
+        if use_valuation_currency and svl_vals_list:
+            # TODO pueden ser diferentes unidades de media
+            svl_vals_list[0]["bypass_currency_valuation"] = True
+            svl_vals_list[0]["value_in_currency"] = (
+                self.price_total - self.purchase_line_id.price_total / self.purchase_line_id.product_qty * self.quantity
+            )
+        return svl_vals_list, aml_vals_list
+
+>>>>>>> FORWARD PORTED
