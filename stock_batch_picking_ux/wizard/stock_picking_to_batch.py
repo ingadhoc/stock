627ac999d23e91820f7cdee970df5f44d7de5ede
Juan Ignacio Carreras <jc@adhoc.com.ar>
2026-05-20 14:55:26 +0000

    [FIX]stock_batch_picking_ux: keep batch partner when attaching pickings to an existing batch
    When attaching pickings to an existing batch, the batch partner should not be
    
    Part-of: ingadhoc/stock#919
    Related: ingadhoc/argentina-sale#300
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- stock_batch_picking_ux/wizard/stock_picking_to_batch.py
+++ stock_batch_picking_ux/wizard/stock_picking_to_batch.py
@@ -7,7 +7,12 @@ class StockPickingToBatch(models.TransientModel):
     _inherit = "stock.picking.to.batch"
 
     def attach_pickings(self):
+        existing_batch = self.batch_id if self.mode == "existing" else self.env["stock.picking.batch"]
+        existing_partner = existing_batch.partner_id
         super().attach_pickings()
+        if self.mode == "existing":
+            existing_batch.partner_id = existing_partner
+            return
         pickings = self.env["stock.picking"].browse(self.env.context.get("active_ids"))
         partner_id = pickings[0].partner_id if pickings else None
         all_same_partner = all(picking.partner_id == partner_id for picking in pickings)
