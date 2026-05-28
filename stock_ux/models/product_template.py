db39be28d9806fce07611f91ee187d898ae4ef07
Juan Ignacio Carreras <jc@adhoc.com.ar>
2026-05-28 17:53:05 +0000

    [ADD] import excel file

--- stock_ux/models/product_template.py
+++ stock_ux/models/product_template.py
@@ -2,11 +2,22 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models
+from odoo import _, api, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
+
+    @api.model
+    def get_import_templates(self):
+        if self.env.context.get("stock_product_template"):
+            return [
+                {
+                    "label": _("Import Template for Products"),
+                    "template": "/stock_ux/static/xls/product_template.xlsx",
+                }
+            ]
+        return super().get_import_templates()
 
     def action_view_stock_move(self):
         self.ensure_one()
